c6df0e8e2bc4be8c3a30129d6181fb96afcd8a03
poncel1968 <40808687+poncel1968@users.noreply.github.com>
2018-07-04 22:25:19 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -9,7 +9,7 @@ edad = parseInt(document.getElementById("edad").value);
 if (edad >= 13 && edad <= 17)
 {
 
-    alert("Es Adolescente");
+    alert(" Es Adolescente");
 
 }
 
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -2,5 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad = parseInt(document.getElementById("edad").value);
+
+if(! (edad >= 13 && edad <= 17))
+{
+
+    alert(" NO Es Adolescente");
+
+}
+
+
 
 }//FIN DE LA FUNCIÓN
