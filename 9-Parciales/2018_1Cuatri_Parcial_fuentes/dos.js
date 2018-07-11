f260e889f1b0c4d51f3bb06f896740771918fc09
poncel1968 <40808687+poncel1968@users.noreply.github.com>
2018-07-10 22:14:15 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
-  
+ var nombre;
+ var localidad;
+
+
+nombre=document.getElementById("elNombre").value;  
+localidad=document.getElementById("laLocalidad").value;
+
+
+
+alert("Uds es " + nombre +" y vive en " + localidad)
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,19 @@
 
 function mostrar()
 {
+var ancho;
+var largo;
+var resultado;
+
+ancho = prompt("Ingresar ancho");
+largo = prompt("Ingresar largo");
+
+ancho = parseInt(ancho);
+largo = parseInt(largo);
+
+resultado = (ancho  + largo) * 2;
+
+alert("El perímetro es  " + resultado);
+
 
 }
