fb2d34e28846f585b544d90b89724c3d9798a62a
poncel1968 <40808687+poncel1968@users.noreply.github.com>
2018-07-11 19:59:35 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,53 @@
 function mostrar()
 {
+var num1;
+var num2;
+var resultado;
+
+num1 = parseInt(prompt("Ingrese el primer número"));
+num2 = parseInt(prompt("Ingrese el segundo número"));
+
+
+
+if (num1 == num2)
+{
+
+    num1=String(num1);
+    num2=String(num2);
+
+    alert( num1 + num2);
+}
+else if (num1>num2)
+{
+
+    resultado = num1 - num2;
+    alert(resultado);
+}
+else if (num1<num2)
+{
+
+    resultado = num1 + num2;
+
+    if (resultado >10)
+    {
+
+        alert("la suma es " + resultado + " y supero el 10");
+
+    }
+    else
+    {
+
+        alert(resultado);
+
+    }
+    
+  
+
 
 }
+
+
+
+
+
+}//fin function
