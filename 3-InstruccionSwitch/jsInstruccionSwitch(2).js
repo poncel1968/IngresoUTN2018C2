262be5ea97964a14aa5adccd28173b060cd57dcb
poncel1968 <40808687+poncel1968@users.noreply.github.com>
2018-07-11 21:41:16 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,27 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+        alert ("Falta para el invierno");
+        break;
+    case "Julio":
+    case "Agosto":
+        alert("Abrigate que hace frío");
+        break;
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+        alert("Ya pasamos el frío, ahora calor");
+        break;
+}
 
 
 
