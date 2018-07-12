eb1032d9cb7eed8a9cea865eb6b167f92955ea11
poncel1968 <40808687+poncel1968@users.noreply.github.com>
2018-07-11 22:05:43 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,7 +5,17 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+switch(mesDelAño)
+{
+
+    case "Febrero":
+        alert (" Este mes no tiene más de 29 días.")
+        break;
+    default:
+        alert(" Este mes tiene 30 o más días ")
+        break;
+
+}	
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -6,7 +6,22 @@ var mesDelAño = document.getElementById('mes').value;
 //alert (mesDelAño);
 	
 	
+switch(mesDelAño)
+{
 
+    case "Febrero":
+        alert("tiene 28 días");
+        break;
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+        alert("Tiene 30 días");
+        break;
+    default :
+        alert("Tiene 31 días");
+
+}
 
 
 }//FIN DE LA FUNCIÓN
