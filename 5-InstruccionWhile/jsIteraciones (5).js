4de48b8de540773f63928ced16ed842dc2128c75
poncel1968 <40808687+poncel1968@users.noreply.github.com>
2018-07-24 21:07:37 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,8 +3,12 @@ function mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
+while(!(sexo == "f" || sexo == "m"))
+{
 
+    sexo = prompt("ingrese f ó m .");
 
+}
 document.getElementById('Sexo').value=sexo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,8 +3,19 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta;
 
+respuesta = prompt("Desea ingresar un numero?");
+
+while(respuesta == "si"){
+
+	acumulador = acumulador + parseInt(prompt("Ingrese un número : "));
+
+	respuesta = prompt("Desea ingresar otro numero?");
+
+	contador+=1;
+
+}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
