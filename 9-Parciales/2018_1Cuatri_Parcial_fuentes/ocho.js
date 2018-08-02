b2640ceea16ddd670dcd8f2fda349cc5f65b972b
poncel1968 <40808687+poncel1968@users.noreply.github.com>
2018-08-01 21:59:02 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,94 @@
 function mostrar()
 {
 
+    var letra;
+    var numero;
+    var cantpares=0;
+    var cantimpares=0;
+    var cantceros=0;
+    var cantpositivos=0;
+    var acumpositivos=0;
+    var acumnegativos=0;
+    var letramaximo;
+    var numeromaximo;
+    var letraminimo;
+    var numerominimo;
+    var respuesta="s";
+    var flag=0;
+
+ while (respuesta != "no"){
+
+     letra=prompt("Ingrese una letra");
+
+     numero=parseInt(prompt("Ingrese un numero "));
+
+     while (numero <-100 || número >100 || isNan(numero)){
+
+         numero = parseInt(prompt("Vuelva a ingresar un numero "));
+
+     }
+
+     if (numero%2==0 ){
+
+         cantpares++;
+
+     }
+     else {
+
+         cantimpares++;
+     }
+  
+
+     if (numero >0 ){
+
+         acumpositivos=acumpositivos+numero;
+         cantpositivos++;
+     }
+     else if (numero < 0) {
+
+         acumnegativos=acumnegativos+numero;
+     }
+     else{
+
+         cantceros++;
+
+     }
+
+
+     if (numero>numeromaximo || flag==0){
+
+         numeromaximo=numero;
+         letramaximo=letra;
+
+     }
+     
+     if( numero < numerominimo || flag==0){
+
+         numerominimo= numero;
+         letraminimo=letra;
+
+         flag=1;
+
+
+     }
+     
+     
+     
+
+        respuesta=prompt("Desea continuar ('no' para salir)");
+     
+
+
+}
+
+
+
+
+
+
+
+
+ }//end while       
+
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,52 @@
 function mostrar()
 {
 
+var nota;
+var sexo;
+var acumu_nota = 0;
+var cont_nota=0;
+var nota_baja;
+var sexo_nota;
+var flag=0;
+
+
+
+for(var i=0;i<5;i++){
+
+do{
+
+    nota=parseInt(prompt("Ingrese la nota"));
+
+}while(nota >10 || nota <0);
+
+do{
+
+    sexo=prompt("Ingrese Sexo");
+    sexo=sexo.toLowerCase();
+
+}while(sexo != "f" && sexo !="m")
+
+acumu_nota=acumu_nota+nota;
+
+cont_nota++;
+
+if(!flag ){
+
+    nota_baja=nota;
+
 }
+else{ 
+
+    if (nota<nota_baja){
+
+        nota_baja
+
+    }
+
+}
+
+
+}
+
+
+}//end function
